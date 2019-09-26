5f635a7f585bebe07558229bf20203e36c6e3cae
JihunJung <raidin79@gmail.com>
2019-09-26 15:37:07 +0900

    Update backpropagation example

--- python/backpropagation.py
+++ python/backpropagation.py
@@ -1,10 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
 from activation_function import *
-
-class Layer:
-    def __init__(self, lr=0.5):
-        self.lr = lr
 
 def MeanSquredError(y, t, prime=False):
     if prime:
@@ -22,97 +19,102 @@ def DrawLossPlot(**loss):
     plt.title('Loss')
     plt.show()
 
+def Forward(x, weigths, act):
+    result = []
 
-def Train(x, t, params, act_func):
-    epoch = 10
-    lr = 0.5
+    for i, w in enumerate(weigths):
+        ele = dict()
+        ele['x'] = x
+        ele['w'] = w
+        ele['y'] = np.dot(x, w)
+        ele['a'] = act(ele['y'])
+        x = ele['a']
+        result.append(ele)
+
+    return result
+
+def Backward(forward, t, act):
+    dout = MeanSquredError(forward[-1]['a'], t, True) # dt/da2
+    forward.reverse()
+    result = []
+
+    for f in forward:
+        dout = dout * act(f['y'], True) # dt/dy2
+        result.append(np.dot(f['x'].T, dout))
+        dout = np.dot(dout, f['w'].T)
+
+    return result
+
+def GradientDescent(weigths, grad, lr=0.5):
+    grad.reverse()
+
+    for i, w in enumerate(weigths):
+        w -= lr * grad[i]
+
+def Train(x, t, params, epoch=200, act_func=Sigmoid, is_visible_loss=False):
     loss = []
 
     for i in range(epoch):
         # Forward
-        ######################################################
-        y1 = np.dot(x, params['w1'])
-        a1 = act_func(y1)
-
-        y2 = np.dot(a1, params['w2'])
-        a2 = act_func(y2)
-
-        out = a2
+        forward = Forward(x, params, act_func)
+        out = forward[-1]['a']
         loss.append(MeanSquredError(out, t))
-        ######################################################
 
         # Backward
-        ######################################################
-        dout = MeanSquredError(out, t, True) # dt/da2
-        dout = dout * act_func(y2, True) # dt/dy2
+        grad = Backward(forward, t, act_func)
 
-        dw2 = np.dot(a1.T, dout)
+        # Gradient Descent
+        GradientDescent(params, grad)
 
-        dout = np.dot(dout, params['w2'].T)
-        dout = dout * act_func(y1, True)
-
-        dw1 = np.dot(x, dout)
-        ######################################################
-
-        params['w2'] -= lr * dw2
-        params['w1'] -= lr * dw1
-
-        # print('[{}]-iter loss :: {}'.format(i+1, loss[-1]))
+        if is_visible_loss : print('[{}]-iter loss :: {}'.format(i+1, loss[-1]))
 
     print('\t[TARGET] ::', t)
     print('\t[OUTPUT] ::', out)
     print('\t[LOSS] ::', loss[-1])
-    # print('==================== W1 ====================\n', params['w1'])
-    # print('==================== W2 ====================\n', params['w2'])
-
     return loss
 
-def InitializeWeight():
-    params = dict()
-    params['w1'] = np.array([[0.15], [0.2]], dtype='float16').reshape(1, 2)
-    # print('- layer-1_weight(shape:{})\n{}'.format(params['w1'].shape, params['w1']))
-    params['w2'] = np.array([[0.40, 0.50],
-                   [0.45, 0.55]], dtype='float16')
-    # print('- layer-2_weight(shape:{})\n{}'.format(params['w2'].shape, params['w2']))
-    return params
+def InitializeWeight(is_fixed=True, in_size=1, hidden_size=2, out_size=2, layer_size=2):
+    weights = []
+    weight_init_std = 0.1
+
+    if is_fixed:
+        weights.append(np.array([[0.15, 0.2]], dtype='float16'))
+        weights.append(np.array([[0.40, 0.50],[0.45, 0.55]], dtype='float16'))
+    else:
+        np.random.seed(777)
+        for i in range(layer_size):
+            weights.append(weight_init_std * np.random.randn(in_size, hidden_size))
+            in_size = hidden_size
+            hidden_size = out_size if i == layer_size-1 else hidden_size
+
+    return weights
 
 def main():
-    t = np.array([[0.2, 0.7]], dtype='float16')
-    print('- Target ::', t.shape, t)
+    t = np.array([0.2, 0.7], dtype='float16')
     x = np.array([[0.7]], dtype='float16')
-    print('- Input(shape:{})\n{}'.format(x.shape, x))
+    print('- Input ::', x.shape)
+    weight_init = InitializeWeight(in_size=x.shape[1], out_size=t.ndim)
+    print('- Weight\n', weight_init)
+
+    return 0
 
     loss_dict = dict()
+    activation_func = [Sigmoid, HyperbolicTangent, Relu, LeakyRelu, ELU]
 
-    # Using Sigmoid Activation
-    params = InitializeWeight()
-    print(' ============= USING SIGMOID ACTIVATION FUNCTION ============= ')
-    loss = Train(x, t, params, Sigmoid)
-    loss_dict['Sigmoid'] = loss
+    # Activation Function을 다르게 적용 할지 유무 결정
+    is_apply_each_act = False
 
-    # Using Relu Activation
-    params = InitializeWeight()
-    print(' ============= USING Tanh ACTIVATION FUNCTION ============= ')
-    loss = Train(x, t, params, HyperbolicTangent)
-    loss_dict['Tanh'] = loss
-
-    # Using Relu Activation
-    params = InitializeWeight()
-    print(' ============= USING RELU ACTIVATION FUNCTION ============= ')
-    loss = Train(x, t, params, Relu)
-    loss_dict['Relu'] = loss
-
-    # Using LeakRelu Activation
-    params = InitializeWeight()
-    print(' ============= USING LeakyRelu ACTIVATION FUNCTION ============= ')
-    loss = Train(x, t, params, LeakyRelu)
-    loss_dict['LeakyRelu'] = loss
-
-    # Using LeakRelu Activation
-    params = InitializeWeight()
-    print(' ============= USING ELU ACTIVATION FUNCTION ============= ')
-    loss = Train(x, t, params, ELU)
-    loss_dict['ELU'] = loss
+    if is_apply_each_act:
+        # Apply Each Activation Function
+        for act in activation_func:
+            print(' ============= USING {} ACTIVATION FUNCTION ============= '.format(act.__name__))
+            loss = Train(x, t, copy.deepcopy(weight_init), act_func=act)
+            loss_dict[act.__name__] = loss
+    else:
+        act = Sigmoid
+        print(' ============= USING {} ACTIVATION FUNCTION ============= '.format(act.__name__))
+        loss = Train(x, t, copy.deepcopy(weight_init), act_func=act)
+        loss_dict[act.__name__] = loss
 
     DrawLossPlot(**loss_dict)
 
