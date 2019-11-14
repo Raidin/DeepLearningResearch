45e68cc2962fddc5b1bff0de31447d4a96865dae
Raidin <raidin79@gmail.com>
2019-11-14 12:40:17 +0900

    Complte Evalucation Metric

--- python/evaluation_metric.py
+++ python/evaluation_metric.py
@@ -10,15 +10,36 @@ class Evaluation():
         self._results = np.array([])
         self._prec = []
         self._rec = []
+        self._eleven_prec = []
+        self._eleven_rec = []
+        self._every_prec = []
+        self._every_rec = []
+        self._AP = 0
+        self.num_of_object = 0
+
+    def GetPrecision(self):
+        return self._prec
+
+    def GetRecall(self):
+        return self._rec
 
     def CreateSampleResult(self):
-        data = pd.read_csv(os.path.join(ROOT_DIR, 'files/evaluation_metric_sample.csv'))
+        data_path = os.path.join(ROOT_DIR, 'files/evaluation_metric_sample_1.csv')
+        data = pd.read_csv(data_path)
+
+        '''
+        * Num of object
+          - sample 1 : 19
+          - sample 2 : 15
+          - sample 2 : 5
+        '''
+        num_of_objects = [19, 15, 5]
+        self.num_of_object = num_of_objects[int(os.path.splitext(os.path.basename(data_path))[0].split('_')[-1]) - 1]
         self._results = np.array(data)
 
     def ComputePrecisionAndRecall(self):
         acc_tp = 0
         acc_fp = 0
-        num_of_object = len(self._results)
         results = self._results
 
         while len(results) > 0:
@@ -30,15 +51,9 @@ class Evaluation():
 
             results = np.delete(results, (idx), axis=0)
             self._prec.append(acc_tp / (acc_tp + acc_fp))
-            self._rec.append(acc_tp / num_of_object)
+            self._rec.append(acc_tp / self.num_of_object)
 
-    def GetPrecision(self):
-        return self._prec
-
-    def GetRecall(self):
-        return self._rec
-
-    def VisualizePlot(self, title='Default', ax=None):
+    def VisualizePlot(self, mode='default', title='default', ax=None):
 
         is_auto_show = False
         if ax is None:
@@ -46,7 +61,20 @@ class Evaluation():
             fig.canvas.set_window_title('Precision & Recall Graph')
             is_auto_show= True
 
-        ax.plot(self._rec, self._prec, linewidth='1.0', linestyle="-")
+        x = self._rec
+        y = self._prec
+        drawstyle = 'default'
+
+        if mode is 'eleven':
+            x = self._eleven_rec
+            y = self._eleven_prec
+            drawstyle = 'steps-post'
+        elif mode is 'every':
+            x = self._every_rec
+            y = self._every_prec
+            drawstyle = 'steps-post'
+
+        ax.plot(x, y, linewidth='1.0', linestyle="-", drawstyle=drawstyle)
         ax.grid(alpha=.4,linestyle='--')
         ax.set_xlim(0, 1.1)
         ax.set_ylim(0, 1.1)
@@ -57,38 +85,60 @@ class Evaluation():
         if is_auto_show:
             plt.show()
 
-    def ComputAveragePrecision():
+    def ComputAveragePrecision(self):
         print('compute Averate Precision')
 
-    def ComputeElevenPointInterpolation():
-        print('eleven point interpolation')
+    def ComputeElevenPointInterpolation(self):
+        self._eleven_rec = np.arange(0, 1.1, 0.1)
+        group = np.stack([evaluation.GetPrecision(), evaluation.GetRecall()], axis=1)
+        for i in self._eleven_rec:
+            samples = np.where(group[:, 1] >= i, group[:,0], 0)
+            self._eleven_prec.append(np.max(samples))
 
-    def ComputeEveryPointInterpolation():
-        print('every point interpolation')
+        self._AP = np.mean(self._eleven_prec)
+        print('11-point interpolation AP :: {:0.2f}%'.format(self._AP * 100))
+
+    def ComputeEveryPointInterpolation(self):
+        self._every_rec = np.unique(self._rec)
+        group = np.stack([self._prec, self._rec], axis=1)
+        for i in self._every_rec:
+            samples = np.where(group[:, 1] >= i, group[:,0], 0)
+            self._every_prec.append(np.max(samples))
+
+        # Compute AP
+        self._every_rec = np.insert(self._every_rec, 0, 0.0)
+        self._every_prec = np.insert(self._every_prec, 0, self._every_prec[0])
+        self._AP = 0
+        for i in range(1, len(self._every_rec)):
+            self._AP += (self._every_rec[i] - self._every_rec[i-1]) * self._every_prec[i-1]
+        # Area under curve(AUC)
+        print('every-point interpolation AP :: {:0.2f}%'.format(self._AP * 100))
 
 
 if __name__ == '__main__':
+    # Init Evaluation
     evaluation = Evaluation()
+    # Create Sample data
     evaluation.CreateSampleResult()
+    # Copmute Default precisin and recall
     evaluation.ComputePrecisionAndRecall()
 
-    print(' - Precision\n', evaluation.GetPrecision())
-    print('- Recall\n', evaluation.GetRecall())
+    # print(' - Precision\n', evaluation.GetPrecision())
+    # print(' - Recall\n', evaluation.GetRecall())
 
-    interval = np.arange(0, 1.1, 0.1)
-    r = []
-    tmp = np.stack([evaluation.GetPrecision(), evaluation.GetRecall()], axis=1)
-    # print(tmp[:,0][np.where(tmp[:, 1] > 0.1)])
-    for i in interval:
-        a = np.where(tmp[:, 1] >= i, tmp[:,0], 0)
-        r.append(np.max(a))
-    print(r)
+    fig, ax = plt.subplots(3, 1, figsize=(5, 10))
+    fig.canvas.set_window_title('Precision & Recall Graph')
+    # Display default precision and recall graph
+    evaluation.VisualizePlot(ax=ax[0])
 
-    rnp = np.array(r)
-    plt.plot(interval, r)
+    # Compute 11-point interpolation
+    evaluation.ComputeElevenPointInterpolation()
+    # Display 11-point interpolation precision and recall graph
+    evaluation.VisualizePlot(ax=ax[1], mode='eleven', title='11-point interpolation')
+
+    # Compute every-point interpolation
+    evaluation.ComputeEveryPointInterpolation()
+    evaluation.VisualizePlot(ax=ax[2], mode='every', title='Every-point interpolation')
+
+    plt.tight_layout()
     plt.show()
-    # fig, ax = plt.subplots(1, 3, figsize=(10,5))
-    # fig.canvas.set_window_title('Precision & Recall Graph')
-    # evaluation.VisualizePlot()
-
-    # plt.show()
